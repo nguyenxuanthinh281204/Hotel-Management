294a924c247acef97f3c8a048f4db1b97c6f34a8
thinhnguyenxuan <ngxthinh271@gmail.com>
2026-05-10 23:52:27 +0700

    create backend project

---
+++ XuanThinhHotel/src/test/java/com/xuanthinhdev/XuanThinhHotel/XuanThinhHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuanthinhdev.XuanThinhHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XuanThinhHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
