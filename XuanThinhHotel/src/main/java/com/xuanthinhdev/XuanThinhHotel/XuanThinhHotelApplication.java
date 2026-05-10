ab31c7b121e8f274c863bfb89a032b70b89da6dd
thinhnguyenxuan <ngxthinh271@gmail.com>
2026-05-10 23:52:27 +0700

    create backend project

---
+++ XuanThinhHotel/src/main/java/com/xuanthinhdev/XuanThinhHotel/XuanThinhHotelApplication.java
@@ -0,0 +1,13 @@
+package com.xuanthinhdev.XuanThinhHotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XuanThinhHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XuanThinhHotelApplication.class, args);
+	}
+
+}
