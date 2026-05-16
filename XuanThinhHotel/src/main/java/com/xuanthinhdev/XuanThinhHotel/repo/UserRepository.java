140b85aa2e875a1ab231647b6257fd3695975c1b
thinhnguyenxuan <ngxthinh271@gmail.com>
2026-05-16 14:24:17 +0700

    Add repositories

---
+++ XuanThinhHotel/src/main/java/com/xuanthinhdev/XuanThinhHotel/repo/UserRepository.java
@@ -0,0 +1,13 @@
+package com.xuanthinhdev.XuanThinhHotel.repo;
+
+import com.xuanthinhdev.XuanThinhHotel.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existByEmail(String email);
+
+    Optional<User> findByEmail(String email);
+}
