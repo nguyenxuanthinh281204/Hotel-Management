307336b914f5f63a386793df6862badf10edbd14
thinhnguyenxuan <ngxthinh271@gmail.com>
2026-06-13 22:37:23 +0700

    Add: write more about service for project

---
+++ XuanThinhHotel/src/main/java/com/xuanthinhdev/XuanThinhHotel/service/interfac/IUserService.java
@@ -0,0 +1,23 @@
+package com.xuanthinhdev.XuanThinhHotel.service.interfac;
+
+import com.xuanthinhdev.XuanThinhHotel.dto.LoginRequest;
+import com.xuanthinhdev.XuanThinhHotel.dto.Response;
+import com.xuanthinhdev.XuanThinhHotel.entity.User;
+
+public interface IUserService {
+    Response register(User user);
+
+    Response login(LoginRequest loginRequest);
+
+    Response getAllUsers();
+
+    Response getUserBookingHistory(String userId);
+
+    Response deleteUser(String userId);
+
+    Response getUserById(String userId);
+
+    Response getMyInfo(String userID);
+
+
+}
